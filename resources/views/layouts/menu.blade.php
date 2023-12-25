52d8b98502a3069b1f68be14c46fd3034d421a43
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2023-12-25 23:24:23 +0700

    takeout sidebar submenu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,60 +1,40 @@
 <ul class="metismenu" id="menu">
     <li>
-        <a class="has-arrow" href="/" aria-expanded="false">
+        <a href="/" aria-expanded="false">
             <i class="icon-speedometer menu-icon"></i><span class="nav-text">Dashboard</span>
         </a>
     </li>
     @if (in_array(Auth::user()->role, ['super']))
     <li>
-        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+        <a href="/user" aria-expanded="false">
             <i class="icon-user menu-icon"></i><span class="nav-text">User Data</span>
         </a>
-        <ul aria-expanded="false">
-            <li><a href="/user">View Data</a></li>
-            <li><a href="#">Create New</a></li>
-        </ul>
     </li>
     @endif
     @if (in_array(Auth::user()->role, ['super', 'ehs']))
     <li>
-        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+        <a href="/characteristic" aria-expanded="false">
             <i class="icon-shield menu-icon"></i><span class="nav-text">Characteristic</span>
         </a>
-        <ul aria-expanded="false">
-            <li><a href="/characteristic">View Data</a></li>
-            <li><a href="#">Create New</a></li>
-        </ul>
     </li>
     @endif
     <li>
-        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+        <a href="/master_bk" aria-expanded="false">
             <i class="icon-chemistry menu-icon"></i><span class="nav-text">Master Bahan Kimia</span>
         </a>
-        <ul aria-expanded="false">
-            <li><a href="/master_bk">View Data</a></li>
-            <li><a href="#">Create New</a></li>
-        </ul>
     </li>
     @if (in_array(Auth::user()->role, ['super', 'waho', 'purchasing']))
     <li>
-        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+        <a href="/ldk" aria-expanded="false">
             <i class="icon-list menu-icon"></i><span class="nav-text">Lembar Data Keselamatan</span>
         </a>
-        <ul aria-expanded="false">
-            <li><a href="/ldk">View Data</a></li>
-            <li><a href="#">Create New</a></li>
-        </ul>
     </li>
     @endif
     @if (in_array(Auth::user()->role, ['super', 'waho', 'ehs']))
     <li>
-        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+        <a href="/location" aria-expanded="false">
             <i class="icon-location-pin menu-icon"></i><span class="nav-text">Locations</span>
         </a>
-        <ul aria-expanded="false">
-            <li><a href="/location">View Data</a></li>
-            <li><a href="#">Create New</a></li>
-        </ul>
     </li>
     @endif
 </ul>
