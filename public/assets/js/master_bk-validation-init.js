9a2fc34b373614eb7f476a9524cb7e3cd2aeffe6
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2023-12-26 18:17:16 +0700

    fix validation

--- public/assets/js/master_bk-validation-init.js
+++ public/assets/js/master_bk-validation-init.js
@@ -39,16 +39,14 @@ jQuery("#editForm").validate({
         jQuery(e).closest(".form-group").removeClass("is-invalid"), jQuery(e).remove();
     },
     rules: {
-        "edit_material_number": { required: !0, minlength: 5 },
-        "edit_material_desc": { required: !0, minlength: 5 },
-        "edit_maker": { required: !0, minlength: 2 },
-        // "edit_ldk_fr_maker": { required: !0 },
+        "material_number": { required: !0, minlength: 5 },
+        "material_desc": { required: !0, minlength: 5 },
+        "maker": { required: !0, minlength: 2 },
     },
     messages: {
-        "edit_material_number": { required: "Please enter material name", minlength: "Your material name must consist of at least 5 characters" },
-        "edit_material_desc": { required: "Please enter material description", minlength: "Material description must consist of at least 5 characters" },
-        "edit_maker": { required: "Please enter maker name", minlength: "Maker name must consist of at least 2 characters" },
-        // "edit_ldk_fr_maker": { required: "Please provide LDK doc from maker"},
+        "material_number": { required: "Please enter material name", minlength: "Your material name must consist of at least 5 characters" },
+        "material_desc": { required: "Please enter material description", minlength: "Material description must consist of at least 5 characters" },
+        "maker": { required: "Please enter maker name", minlength: "Maker name must consist of at least 2 characters" },
     },
 });
 
@@ -66,14 +64,13 @@ jQuery("#reviewForm").validate({
         jQuery(e).closest(".form-group").removeClass("is-invalid"), jQuery(e).remove();
     },
     rules: {
-        "review_material_number": { required: !0, minlength: 5 },
+        "material_number": { required: !0, minlength: 5 },
         "approve_reject": { required: !0 },
-        "review_comment": { required: !0, minlength: 2 },
+        "comment": { required: !0, minlength: 2 },
     },
     messages: {
-        "review_material_number": { required: "Please enter material name", minlength: "Your material name must consist of at least 5 characters" },
+        "material_number": { required: "Please enter material name", minlength: "Your material name must consist of at least 5 characters" },
         "approve_reject": { required: "Approve / Reject is required"},
-        "review_comment": { required: "Please enter comment if reject", minlength: "Review comment must consist of at least 2 characters" },
-        // "edit_ldk_fr_maker": { required: "Please provide LDK doc from maker"},
+        "comment": { required: "Please enter comment if reject", minlength: "Review comment must consist of at least 2 characters" },
     },
 });
